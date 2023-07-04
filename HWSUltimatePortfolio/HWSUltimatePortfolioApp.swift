d062cb5b7a49aa34a364be6b10e0c5d77d00b131
Filip Miladinovic <filip982@gmail.com>
2023-07-04 23:50:58 +0200

    Initial Commit

---
+++ HWSUltimatePortfolio/HWSUltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWSUltimatePortfolioApp.swift
+//  HWSUltimatePortfolio
+//
+//  Created by Filip Miladinovic on 04.07.23.
+//
+
+import SwiftUI
+
+@main
+struct HWSUltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
